ad082ba8d15431d4336362de4cf8828e8660ffaf
emmanuelle dupas-mahe <emmanuelle.dupas-mahe@laplateforme.io>
2025-09-30 16:17:32 +0200

    module-connexion#8

--- deconnexion.php
+++ deconnexion.php
@@ -1,13 +1,13 @@
 <?php
-// 1. Démarrer la session. 
+
 session_start();
 
-// 2. Supprimer toutes les variables de session.
+
 $_SESSION = array();
 
-// 3. Si l'on souhaite détruire complètement la session, 
+// Pour détruire complètement la session, 
 // il faut aussi détruire le cookie de session.
-// Note : Cela rendra obsolète le 'session_id' qui est dans le navigateur.
+
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -16,10 +16,10 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// 4. Finalement, détruire la session.
+// destruction de la session.
 session_destroy();
 
-// 5. Rediriger l'utilisateur vers la page d'accueil (index.php)
+// vers la page d'accueil (index.php)
 header('Location: index.php');
 exit();
 ?>
